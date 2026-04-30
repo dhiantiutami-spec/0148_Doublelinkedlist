ecff01f103c2cc245c2f0b58a9b5567a5b72c97d
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-04-30 12:29:12 +0700

    menambah prosedur revTraverse

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -126,5 +126,27 @@ public:
             current = current->next;
         }
     }
-    
+    void revTraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        Node *current = START;
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+
+        cout << "\nRecords in descending order:\n";
+
+        while (current != NULL)
+        {
+            cout << current->noMhs << " ";
+            current = current->prev;
+        }
+    }
+
 }
